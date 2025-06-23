2d8f979c2e5a7512de25dd630bb56d1d2c741b21
Brendan Ashworth <brendan.ashworth@me.com>
2025-06-23 16:23:56 -0700

    build with posthog if its available

---
+++ frontendts/src/lib/posthog.ts
@@ -0,0 +1,17 @@
+import posthog from 'posthog-js'
+
+export const initPostHog = () => {
+  const apiKey = import.meta.env.VITE_POSTHOG_API_KEY
+  
+  if (apiKey) {
+    posthog.init(apiKey, { 
+      api_host: 'https://us.i.posthog.com', 
+      defaults: '2025-05-24' 
+    })
+    return true
+  }
+  
+  return false
+}
+
+export { posthog }
