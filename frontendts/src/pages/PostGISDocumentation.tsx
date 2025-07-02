53d08b4eb154b93f605d2290058d9c48a1cef5dc
Brendan Ashworth <brendan.ashworth@me.com>
2025-07-02 15:08:15 -0600

    asking questions of postgis wiki

--- frontendts/src/pages/PostGISDocumentation.tsx
+++ frontendts/src/pages/PostGISDocumentation.tsx
@@ -277,6 +277,21 @@ If documentation generation fails, this indicates the database connection detail
                   )}
                 </div>
               </div>
+
+              <div className="border-l-4 border-blue-500 bg-blue-50 dark:bg-blue-950 p-4 mb-6 mx-8">
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <svg className="h-5 w-5 text-blue-400 dark:text-blue-300" viewBox="0 0 20 20" fill="currentColor">
+                      <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
+                    </svg>
+                  </div>
+                  <div className="ml-3">
+                    <p className="text-sm text-blue-700 dark:text-blue-200">
+                      <strong>Have questions?</strong> Open any map that is connected to this PostGIS database, and Kue will be able to answer questions based on this article.
+                    </p>
+                  </div>
+                </div>
+              </div>
             </ScrollArea>
           </div>
 
